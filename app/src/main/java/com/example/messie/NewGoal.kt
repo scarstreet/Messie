79aeb2199f69103483992bcef852e9c4b48738c9
Astrid <scarlettstrid@gmail.com>
2022-11-20 17:15:50 +0800

    Added new activity files TODO

--- app/src/main/java/com/example/messie/NewGoal.kt
+++ app/src/main/java/com/example/messie/NewGoal.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 // THIS IS AN ACTIVITY
 
-class MainActivity : AppCompatActivity() {
+class NewGoal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_goal)
     }
 }
