958fcf4d302a2592b7b0e699db07fdc88bf1d623
Astrid <scarlettstrid@gmail.com>
2022-11-28 15:24:53 +0800

    added navdrawer and functional navbar

--- app/src/main/java/com/example/messie/MainActivity.kt
+++ app/src/main/java/com/example/messie/MainActivity.kt
@@ -4,33 +4,37 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.messie.databinding.ActivityMainBinding
 
 // THIS IS AN ACTIVITY
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(Home())
-        binding.navBar.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home -> replaceFragment(Home())
-                R.id.goalList -> replaceFragment(GoalList())
-                R.id.statistics -> replaceFragment(Home())
-                R.id.settings -> replaceFragment(GoalList())
-                else -> replaceFragment(Home())
-            }
-            true
-        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.fragment_home), binding.drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.navView.setupWithNavController(navController)
+        binding.navBar.setupWithNavController(navController)
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp(appBarConfiguration)
     }
 }
